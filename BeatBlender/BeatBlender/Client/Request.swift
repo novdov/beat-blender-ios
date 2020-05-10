d0ff3c9d8813e29f2d850602b6a0ac4aec963a57
novdov <rpwjd56@gmail.com>
2020-05-11 08:57:55 +0900

    sty) Format codes

--- BeatBlender/BeatBlender/Client/Request.swift
+++ BeatBlender/BeatBlender/Client/Request.swift
@@ -11,11 +11,11 @@ struct BeatBlenderRequest: BeatBlenderCodable {
 	/// Encode and stringify `BeatBlenderCodable` protocol
 	/// - Returns: Strinified json data.
 	func stringify() -> String? {
-        let encoder = JSONEncoder()
-        let encoded = try? encoder.encode(self)
-        if let encoded = encoded, let jsonString = String(data: encoded, encoding: .utf8) {
-            return jsonString
-        }
-        return nil
-    }
+		let encoder = JSONEncoder()
+		let encoded = try? encoder.encode(self)
+		if let encoded = encoded, let jsonString = String(data: encoded, encoding: .utf8) {
+			return jsonString
+		}
+		return nil
+	}
 }
