13cec14bc111c65299573941e330123ee1d7a97a
novdov <rpwjd56@gmail.com>
2020-05-11 08:34:28 +0900

    fea) Change indentation

--- BeatBlender/BeatBlenderTests/Client/RequestTests.swift
+++ BeatBlender/BeatBlenderTests/Client/RequestTests.swift
@@ -4,12 +4,10 @@ import XCTest
 @testable import BeatBlender
 
 class RequestTests: XCTestCase {
-	func testStringfy() throws {
-		let beatBlenderRequest = BeatBlenderRequest(numSamples: 1, temperature: 0.5)
-		
-		let expectedJsonString: String = "{\"numSamples\":1,\"temperature\":0.5}"
-		
-		let jsonString = beatBlenderRequest.stringify()
-		XCTAssertEqual(jsonString, expectedJsonString)
-	}
+    func testStringfy() throws {
+        let beatBlenderRequest = BeatBlenderRequest(numSamples: 1, temperature: 0.5)
+        let expectedJsonString: String = "{\"numSamples\":1,\"temperature\":0.5}"
+        let jsonString = beatBlenderRequest.stringify()
+        XCTAssertEqual(jsonString, expectedJsonString)
+    }
 }
