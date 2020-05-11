88ff6999805eff07a29d7b22a135b974a0b6ab5d
novdov <rpwjd56@gmail.com>
2020-05-11 09:09:40 +0900

    sty) Format files

--- BeatBlender/BeatBlender/Client/Request.swift
+++ BeatBlender/BeatBlender/Client/Request.swift
@@ -7,15 +7,15 @@ protocol BeatBlenderCodable: Codable {
 struct BeatBlenderRequest: BeatBlenderCodable {
     let numSamples: Int?
     let temperature: Float64?
-	
-	/// Encode and stringify `BeatBlenderCodable` protocol
-	/// - Returns: Strinified json data.
-	func stringify() -> String? {
-		let encoder = JSONEncoder()
-		let encoded = try? encoder.encode(self)
-		if let encoded = encoded, let jsonString = String(data: encoded, encoding: .utf8) {
-			return jsonString
-		}
-		return nil
-	}
+
+    /// Encode and stringify `BeatBlenderCodable` protocol
+    /// - Returns: Strinified json data.
+    func stringify() -> String? {
+        let encoder = JSONEncoder()
+        let encoded = try? encoder.encode(self)
+        if let encoded = encoded, let jsonString = String(data: encoded, encoding: .utf8) {
+            return jsonString
+        }
+        return nil
+    }
 }
--- BeatBlender/BeatBlenderTests/BeatBlenderTests.swift
+++ BeatBlender/BeatBlenderTests/BeatBlenderTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2020 novdov. All rights reserved.
 //
 
-import XCTest
 @testable import BeatBlender
+import XCTest
 
 class BeatBlenderTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,9 +25,8 @@ class BeatBlenderTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
