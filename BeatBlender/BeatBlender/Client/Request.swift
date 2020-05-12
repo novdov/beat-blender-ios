5fdf77d8b87411d2c07338e692a1e82c296c009b
novdov <rpwjd56@gmail.com>
2020-05-13 08:53:16 +0900

    fea) Refactor Request

--- BeatBlender/BeatBlender/Client/Request.swift
+++ BeatBlender/BeatBlender/Client/Request.swift
@@ -1,21 +1,59 @@
 import Foundation
 
-protocol BeatBlenderCodable: Codable {
-    func stringify() -> String?
+enum APIError: Error {
+    case responseError
+    case encodingError
+    case decodingError
 }
 
-struct BeatBlenderRequest: BeatBlenderCodable {
-    let numSamples: Int?
-    let temperature: Float64?
+struct BeatBlenderRequestBody: Codable {
+    let numSamples: Int
+    let temperature: Float64
+}
 
-    /// Encode and stringify `BeatBlenderCodable` protocol
-    /// - Returns: Strinified json data.
-    func stringify() -> String? {
-        let encoder = JSONEncoder()
-        let encoded = try? encoder.encode(self)
-        if let encoded = encoded, let jsonString = String(data: encoded, encoding: .utf8) {
-            return jsonString
+struct BeatBlenderRequest {
+    var session: URLSession = URLSession.shared
+    let baseUrl: String
+
+    init(baseUrl: String) {
+        self.baseUrl = baseUrl
+    }
+
+    private func getUrlRequest(endpoint: String) -> URLRequest {
+        let url = baseUrl + endpoint
+        guard let requestUrl = URL(string: url) else { fatalError() }
+        let request = URLRequest(url: requestUrl)
+        return request
+    }
+
+    func post(
+        endpoint: String,
+        requestBody: BeatBlenderRequestBody,
+        completion: @escaping (Result<[String: Any], APIError>) -> Void
+    ) {
+        var request = getUrlRequest(endpoint: endpoint)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        guard let encodedHttpBody = try? JSONEncoder().encode(requestBody) else {
+            completion(.failure(.encodingError))
+            return
         }
-        return nil
+        request.httpBody = encodedHttpBody
+        let task = session.dataTask(with: request) { data, response, _ in
+            guard let httpResponse = response as? HTTPURLResponse, 200 ... 201 ~= httpResponse.statusCode,
+                let jsonData = data else {
+                completion(.failure(.responseError))
+                return
+            }
+
+            do {
+                let sampleData = try JSONSerialization.jsonObject(with: jsonData, options: [])
+                completion(.success(sampleData as! [String: Any]))
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }
+        task.resume()
     }
 }
