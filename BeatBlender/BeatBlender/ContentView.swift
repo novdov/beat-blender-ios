5f868f4e9e615fad954febefea7c0559d71bf4ba
novdov <rpwjd56@gmail.com>
2020-05-14 17:28:46 +0900

    .gitignore fix

---
+++ BeatBlender/BeatBlender/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  BeatBlender
+//
+//  Created by 김선웅 on 2020/05/09.
+//  Copyright © 2020 novdov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	let spliceRequest = BeatBlenderRequest(baseUrl: "http://localhost:8080/model")
+
+    var body: some View {
+		Button(action: {
+			self.sample()
+		}) {
+			Text("Sample")
+		}
+    }
+
+	func sample() -> Void {
+		let body = BeatBlenderRequestBody(numSamples: 1, temperature: 0.5)
+		spliceRequest.post(endpoint: "/sample", requestBody: body, completion: { result in
+			switch result {
+			case .success(let sampleData):
+				let arr = sampleData["samples"] as! NSArray
+				guard let jsonString: String = stringifyJson(withJSONObject: arr[0] as! [String:Any]) else {
+					return
+				}
+				guard let noteSequence: Tensorflow_Magenta_NoteSequence = jsonToNoteSequence(jsonString: jsonString) else {
+					return
+				}
+				print(noteSequence)
+			case .failure(let error):
+				print(error)
+			}
+		})
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# beat-blender-ios
