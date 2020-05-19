1b8157e0a75828f57d2f3a3a5ad5663f9535012c
novdov <rpwjd56@gmail.com>
2020-05-20 08:05:49 +0900

    fea) Finalize business logic

--- BeatBlender/BeatBlender/Music.swift
+++ BeatBlender/BeatBlender/Music.swift
@@ -1,40 +1,79 @@
 import AVFoundation
 import Foundation
 
+let midiNoteToDrumRowMap: [Int32: Int32] = [
+  36: 0,
+  38: 1,
+  42: 2,
+  46: 3,
+  45: 4,
+  48: 5,
+  50: 6,
+  49: 7,
+  51: 8,
+]
+
+let drumRowToMidiNoteMap: [Int32: Int32] = [
+  0: 36,
+  1: 38,
+  2: 42,
+  3: 46,
+  4: 34,
+  5: 48,
+  6: 50,
+  7: 49,
+  8: 51,
+]
+
+struct DrumEncoderDecoder {
+  func midiNoteToDrumRow(_ midiNote: Int32) -> Int32 {
+    midiNoteToDrumRowMap[midiNote] ?? -1
+  }
+
+  func drumRowtoMidiNote(_ drumRow: Int32) -> Int32 {
+    drumRowToMidiNoteMap[drumRow] ?? -1
+  }
+}
+
 extension Tensorflow_Magenta_NoteSequence {
-    func toMusicSequence() -> MusicSequence? {
-        var musicSequence: MusicSequence?
-        var status = NewMusicSequence(&musicSequence)
-        guard status == OSStatus(noErr) else {
-            print("[\(status)] Error: could not convert Notes to MusicSequence")
-            return nil
-        }
-
-        var track: MusicTrack?
-        status = MusicSequenceNewTrack(musicSequence!, &track)
-        guard status == OSStatus(noErr) else {
-            print("[\(status)] Error: could not create MusicTrack from MusicSequence")
-            return nil
-        }
-
-        var timeStampInSeconds: Float64 = 0
-        for note in notes {
-            print(note)
-            let duration = note.endTime - note.startTime
-            var message = MIDINoteMessage(
-                channel: 10,
-                note: UInt8(note.pitch),
-                velocity: 64,
-                releaseVelocity: 0,
-                duration: Float32(duration)
-            )
-            timeStampInSeconds += duration
-            let timeStamp = MusicTimeStamp(timeStampInSeconds)
-            status = MusicTrackNewMIDINoteEvent(track!, timeStamp, &message)
-            if status != OSStatus(noErr) {
-                print("[\(status)] Error: could not create MIDINoteEvent")
-            }
-        }
-        return musicSequence
+  func toMusicSequence() -> MusicSequence? {
+    var musicSequence: MusicSequence?
+    var status = NewMusicSequence(&musicSequence)
+    guard status == OSStatus(noErr) else {
+      print("[\(status)] Error: could not convert Notes to MusicSequence")
+      return nil
     }
+
+    var track: MusicTrack?
+    status = MusicSequenceNewTrack(musicSequence!, &track)
+    guard status == OSStatus(noErr) else {
+      print("[\(status)] Error: could not create MusicTrack from MusicSequence")
+      return nil
+    }
+
+    var beat = MusicTimeStamp(0.5)
+    for note in notes {
+      // Use quantizedStep as time
+      // Reference: https://github.com/tensorflow/magenta/blob/master/magenta/music/sequences_lib.py#L1680
+      //            let duration = note.quantizedEndStep - note.quantizedStartStep
+      var message = MIDINoteMessage(
+        channel: 10,
+        note: UInt8(note.pitch),
+        velocity: getRandomVelocity(),
+        releaseVelocity: 0,
+        duration: 0.5
+      )
+      //            let timeStampInSeconds = Float64(note.quantizedStartStep)
+      status = MusicTrackNewMIDINoteEvent(track!, beat, &message)
+      if status != OSStatus(noErr) {
+        print("[\(status)] Error: could not create MIDINoteEvent")
+      }
+      beat += 0.5
+    }
+    return musicSequence
+  }
+}
+
+func getRandomVelocity() -> UInt8 {
+  UInt8.random(in: 64...74)
 }
--- BeatBlender/BeatBlender/Request.swift
+++ BeatBlender/BeatBlender/Request.swift
@@ -3,58 +3,62 @@ import Foundation
 typealias ResponseType = [String: Any]
 
 enum APIError: Error {
-    case responseError
-    case encodingError
-    case decodingError
+  case responseError
+  case encodingError
+  case decodingError
 }
 
 struct BeatBlenderRequestBody: Codable {
-    let numSamples: Int
-    let temperature: Float64
+  let numSamples: Int
+  let temperature: Float64
 }
 
 struct BeatBlenderRequest {
-    var session: URLSession = URLSession.shared
-    let baseUrl: String
+  var session: URLSession = URLSession.shared
+  let baseUrl: String
 
-    init(baseUrl: String) {
-        self.baseUrl = baseUrl
+  init(baseUrl: String) {
+    self.baseUrl = baseUrl
+  }
+
+  private func getUrlRequest(endpoint: String) -> URLRequest {
+    let url = baseUrl + endpoint
+    guard let requestUrl = URL(string: url) else { fatalError() }
+    let request = URLRequest(url: requestUrl)
+    return request
+  }
+
+  func post(
+    endpoint: String,
+    requestBody: BeatBlenderRequestBody,
+    completion: @escaping (Result<ResponseType, APIError>) -> Void
+  ) {
+    var request = getUrlRequest(endpoint: endpoint)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+    guard let encodedHttpBody = try? JSONEncoder().encode(requestBody) else {
+      completion(.failure(.encodingError))
+      return
     }
+    request.httpBody = encodedHttpBody
+    let task = session.dataTask(with: request) { data, response, _ in
+      guard let httpResponse = response as? HTTPURLResponse, 200...299 ~= httpResponse.statusCode,
+        let jsonData = data
+      else {
+        completion(.failure(.responseError))
+        return
+      }
 
-    private func getUrlRequest(endpoint: String) -> URLRequest {
-        let url = baseUrl + endpoint
-        guard let requestUrl = URL(string: url) else { fatalError() }
-        let request = URLRequest(url: requestUrl)
-        return request
+      guard
+        let sampleData = try? JSONSerialization.jsonObject(with: jsonData, options: [])
+          as? ResponseType
+      else {
+        completion(.failure(.decodingError))
+        return
+      }
+      completion(.success(sampleData))
     }
-
-    func post(
-        endpoint: String,
-        requestBody: BeatBlenderRequestBody,
-        completion: @escaping (Result<ResponseType, APIError>) -> Void
-    ) {
-        var request = getUrlRequest(endpoint: endpoint)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        guard let encodedHttpBody = try? JSONEncoder().encode(requestBody) else {
-            completion(.failure(.encodingError))
-            return
-        }
-        request.httpBody = encodedHttpBody
-        let task = session.dataTask(with: request) { data, response, _ in
-            guard let httpResponse = response as? HTTPURLResponse, 200 ... 201 ~= httpResponse.statusCode,
-                let jsonData = data else {
-                completion(.failure(.responseError))
-                return
-            }
-
-            guard let sampleData = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? ResponseType else {
-                completion(.failure(.decodingError))
-                return
-            }
-            completion(.success(sampleData))
-        }
-        task.resume()
-    }
+    task.resume()
+  }
 }
